4c6d2161fe1e315e195b2cd44f5053a31d633b22
Nico <nicoclarianabasi1@gmail.com>
2023-10-23 09:37:30 +0200

    inicio pagina malenia

--- app/src/main/java/com/example/eldenrin_final_deverdad/Malenia.java
+++ app/src/main/java/com/example/eldenrin_final_deverdad/Malenia.java
@@ -2,7 +2,11 @@ package com.example.eldenrin_final_deverdad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Malenia extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class Malenia extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.malenia);
+        ImageButton back = findViewById(R.id.backMalenia);
+        back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intentBack = new Intent(Malenia.this, ListaBosses.class);
+                startActivity(intentBack);
+            }
+        });
     }
 }
